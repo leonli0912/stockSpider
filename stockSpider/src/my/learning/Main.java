f9d5a100a287df549a8035f0c6177578bd69e3a1
i074178 <leon.li02@sap.com>
2019-03-20 17:12:28 +0800

    remove unneed charater from api

--- stockSpider/src/my/learning/Main.java
+++ stockSpider/src/my/learning/Main.java
@@ -8,16 +8,19 @@ public class Main {
 
 
     public static void main(String[] args) {
+        String STOCKFIELDS = "name,today_open,lastday_close,today_close,highest,lowest,buy1,sold1,trade_volume,trade_amount,change,date,time";
         UrlHelper urlHelper = new UrlHelper();
         String s = null;
         try {
             ArrayList stockCodes = StockListReader.ReadFile("src/stockList.txt");
-            System.out.println(stockCodes.size());
+
             for(int i=0;i<10;i++){
                 String stockCode = stockCodes.get(i).toString();
                 System.out.println("get data"+stockCode);
                 s = urlHelper.doGet("http://hq.sinajs.cn/list="+stockCode);
-                System.out.print(s);
+                String result = s.substring(s.indexOf("=")+1).replace("\"", "");
+
+                System.out.print(result);
             }
 
         } catch (Exception e) {
